41cf80e1a7a0897982530cd265eb56236fce7e4d
jung un Kim <azuremous@gmail.com>
2013-08-22 09:55:34 +0900

    add start fuction

--- example/App.cpp
+++ example/App.cpp
@@ -41,7 +41,7 @@ void testApp::mouseDragged(int x, int y, int button){
 
 //--------------------------------------------------------------
 void testApp::mousePressed(int x, int y, int button){
-
+	tr.start();
 }
 
 //--------------------------------------------------------------
